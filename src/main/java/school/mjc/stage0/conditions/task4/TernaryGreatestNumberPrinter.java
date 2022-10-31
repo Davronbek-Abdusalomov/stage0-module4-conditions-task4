f4b2e1a33bfe735c86f67cf8fab99f4a4eacccc0
Khusniddinova <2110146@newuzbekistanuniversity.uz>
2022-10-31 23:19:24 +0500

    ternary

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.conditions.task4;
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second)
     {
-        int max= first>second ? first:second;
-        System.out.println(max);
+        int max = first>second ? first:second;
+
     }
 }
